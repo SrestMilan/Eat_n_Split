13d8508df41b034d922d99f3d83f38af1ededdd6
SrestMilan <shrestham780@gmail.com>
2023-11-03 11:52:41 +0545

    showing add form

--- eat-n-split/src/components/App.js
+++ eat-n-split/src/components/App.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import FriendList from "./FriendList";
 import AddFriendForm from "./AddFriendForm";
 import Button from "./Button";
 import SplitBill from "./SplitBill";
 
 export default function App() {
+  const [showFriend, setShowFriend] = useState(false);
+  function handleAddFriend() {
+    setShowFriend((show) => !show);
+  }
   return (
     <div className="app">
       <div className="sidebar">
         <FriendList />
-        <AddFriendForm />
-        <Button>Add Friend</Button>
+        {showFriend && <AddFriendForm />}
+        <Button onClick={handleAddFriend}>
+          {showFriend ? "Close" : "Add Friend"}
+        </Button>
       </div>
       <SplitBill />
     </div>
