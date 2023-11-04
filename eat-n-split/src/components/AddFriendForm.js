4aed104b7a4c98c40186256a95f78978828a2337
SrestMilan <shrestham780@gmail.com>
2023-11-04 13:44:28 +0545

    adding friend functionality

--- eat-n-split/src/components/AddFriendForm.js
+++ eat-n-split/src/components/AddFriendForm.js
@@ -1,11 +1,40 @@
+import { useState } from "react";
 import Button from "./Button";
-function AddFriendForm() {
+function AddFriendForm({ onHandleFriendList }) {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/48");
+
+  function friendFormHandle(e) {
+    e.preventDefault();
+    if (!name || !image) return;
+    const id = crypto.randomUUID();
+    const newFriend = {
+      id,
+      name,
+      image: `${image}?=${id}`,
+      balance: 0,
+    };
+    onHandleFriendList(newFriend);
+    console.log(newFriend);
+    setName("");
+    setImage("https://i.pravatar.cc/48");
+  }
+
   return (
-    <form className="form-add-friend">
+    <form className="form-add-friend" onSubmit={friendFormHandle}>
       <label>👫Friend name</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
       <label>🌄 Image Url</label>
-      <input type="text" alt="friend image" />
+      <input
+        type="text"
+        alt="friend image"
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+      />
       <Button>Add</Button>
     </form>
   );
--- eat-n-split/src/components/App.js
+++ eat-n-split/src/components/App.js
@@ -4,16 +4,43 @@ import AddFriendForm from "./AddFriendForm";
 import Button from "./Button";
 import SplitBill from "./SplitBill";
 
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
 export default function App() {
   const [showFriend, setShowFriend] = useState(false);
+  const [friends, setFriends] = useState(initialFriends);
   function handleAddFriend() {
     setShowFriend((show) => !show);
+  }
+
+  function handlFriendList(friend) {
+    setFriends((friends) => [...friends, friend]);
+    setShowFriend(false);
   }
   return (
     <div className="app">
       <div className="sidebar">
-        <FriendList />
-        {showFriend && <AddFriendForm />}
+        <FriendList friendList={friends} />
+        {showFriend && <AddFriendForm onHandleFriendList={handlFriendList} />}
         <Button onClick={handleAddFriend}>
           {showFriend ? "Close" : "Add Friend"}
         </Button>
